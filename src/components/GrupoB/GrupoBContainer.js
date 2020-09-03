504e950a1becdd0859a57e2e1ea11bcd459cb7f4
Steven Bedoya <steven@jayacompany.com>
2020-09-03 13:48:13 -0500

    Add url query id catch

--- src/components/GrupoB/GrupoBContainer.js
+++ src/components/GrupoB/GrupoBContainer.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Form from './Form';
 
-export default function GrupoBContainer () {
+export default function GrupoBContainer (props) {
+
   return (
     <div>
-     <Form productoId='5f2d72976b03f9cecbe30204'/>
+      {/*<Form productoId='5f2d72976b03f9cecbe30204' />*/}
+      {/*<Form productoId={props.match.params.id} />*/}
+      <Form productoId={props.location.search.slice(4)}/>
     </div>
   )
 
