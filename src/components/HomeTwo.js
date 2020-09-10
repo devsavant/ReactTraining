f1f6841adb82d01a96b1fd0408fa15069b332c14
Martín Melo <martin.melo.dev.97@gmail.com>
2020-09-10 09:27:23 -0500

    Redux

--- src/components/HomeTwo.js
+++ src/components/HomeTwo.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { transformTokenAction } from '../redux/userDuck'
+import { transformTokenAction, transformDisplayNameAction } from '../redux/userDuck'
 
-const HomeTwo = ({ token, displayName, transformTokenAction }) => {
+const HomeTwo = ({ token, displayName, transformTokenAction, transformDisplayNameAction }) => {
 
     const onClick = () => {
         transformTokenAction("lo que sea")
     }
 
-    return ( 
+    const handleChange = (event) => {
+        transformDisplayNameAction(event.target.value)
+    }
+
+    return (
         <div>
             <h2>Home with redux, welcome {displayName}</h2>
             <h3>{token}</h3>
+            <label for="displayName">Tu nombrecito:</label>
+            <input type="text" name="displayName" id="displayname" onChange={handleChange} value={displayName} />
             <button onClick={onClick}>
                 Transform Token
             </button>
@@ -27,5 +33,5 @@ function mapStateToProps({user:{displayName, token}}) {
 }
 
 // agregar un input para que escribamos el nuevo token y usar el valor para reescribir el token
- 
-export default connect(mapStateToProps, {transformTokenAction})(HomeTwo);
+
+export default connect(mapStateToProps, { transformTokenAction, transformDisplayNameAction })(HomeTwo);
