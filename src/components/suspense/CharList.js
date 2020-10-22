ca94e75d34aa82765d2eda792d0647beef4fcf93
Martín Melo <martin.melo.dev.97@gmail.com>
2020-10-22 16:19:01 -0500

    Showing characters in CharList using suspensify

--- src/components/suspense/CharList.js
+++ src/components/suspense/CharList.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import suspensify from './suspensify'
+
+const url = 'https://rickandmortyapi.com/api/character'
+const allCharacters = suspensify(
+  fetch(url).then(response => response.json())
+)
 
 const CharList = () => {
+  const characters = allCharacters.read().results
     return (
         <>
+        <h2>Personajes de Rick & Morty</h2>
+        {characters.map(char =>
+          <p key={char.id}>{char.name}</p>
+        )}
         </>
       );
 }
- 
+
 export default CharList;
