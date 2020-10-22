f6f493405f80bf0255cfc6de8209bfe7ac0ccfac
Steven Bedoya <steven@jayacompany.com>
2020-10-22 17:45:29 -0500

    Suspensify challenge - Steven

--- src/components/suspense/CharList.js
+++ src/components/suspense/CharList.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import suspensify from './suspensify'
+
+const chars = suspensify(fetch('https://rickandmortyapi.com/api/character/').then(r=>r.json()))
 
 const CharList = () => {
+    const squareLength = 30;
+    const listChars = chars.read().results.map((char) =>
+        <div key={char.id.toString()}>
+            <img style={{maxWidth: squareLength+'px', maxHeight: squareLength+'px'}} src={char.image} alt={char.name} />{char.name}
+        </div>
+    );
+
     return (
-        <>
-        </>
+        <div>
+            <div>{listChars}</div>
+        </div>
       );
 }
  
