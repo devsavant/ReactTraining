3d5c5b5fe82d7e866eed61e59e543512a22a14cf
Alejandro Heredia <alex.heredia.barbosa@gmail.com>
2020-10-22 17:50:58 -0500

    Display all chars

--- src/components/suspense/CharDisplay.js
+++ src/components/suspense/CharDisplay.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import suspensify from './suspensify'
 
-const rick = suspensify(fetch('https://rickandmortyapi.com/api/character/1').then(r=>r.json()))
+const rick = suspensify(fetch('https://rickandmortyapi.com/api/character').then(r=>r.json()))
 
 
 const CharDisplay = () => {
+
+    function getElements(){
+        return rick.read().results.map(char => returnElement(char))
+    }
+
+    function returnElement(char){
+        return <span key={char.id}><img src={char.image} alt={char.image} width={100} height={100}/><p>{char.name}</p></span>
+    }
+
     return ( 
         <div>
-            <h2 >Personaje : {rick.read().name}</h2>
+            <div style={{display: "flex", flexDirection: "row", width: "80%"}}>{getElements()}</div>
         </div>
      );
 }
